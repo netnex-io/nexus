b7be980c555ce5e839d29bf3e9c177f427bb53ae
Tyler Harold <tyler@programmer.net>
2024-07-24 18:09:58 -0400

    feat(server): add ConnectionLimit, IdleTimeout & EnableCompression to server cfg

--- nexus/server.go
+++ nexus/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -12,21 +13,24 @@ import (
 )
 
 type ServerConfig struct {
-	AllowedOrigins []string
-	MaxMessageSize int64
-	CertFile       string
-	KeyFile        string
+	AllowedOrigins    []string
+	MaxMessageSize    int64
+	CertFile          string
+	KeyFile           string
+	ConnectionLimit   int
+	IdleTimeout       int
+	EnableCompression bool
 }
 
 func DefaultServerConfig() ServerConfig {
 	return ServerConfig{
-		// Allow all origins by default
-		AllowedOrigins: []string{"*"},
-		// Default max message size (1KB)
-		MaxMessageSize: 1024,
-		// Cert & Key Files
-		CertFile: "",
-		KeyFile:  "",
+		AllowedOrigins:    []string{"*"}, // Allow all origins by default
+		MaxMessageSize:    1024,          // Default max message size (1KB)
+		CertFile:          "",            // Default empty string for CertFile
+		KeyFile:           "",            // Default empty string for KeyFile
+		ConnectionLimit:   0,             // Default unlimited connections
+		IdleTimeout:       0,             // Default idle timeout in seconds
+		EnableCompression: true,          // Enable permessage-deflate compression
 	}
 }
 
@@ -36,6 +40,9 @@ type Server struct {
 
 	// Config
 	Config ServerConfig
+
+	// Active connection count
+	connections int
 }
 
 func NewServer(config ServerConfig) *Server {
@@ -61,6 +68,7 @@ func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 			return false
 		},
+		EnableCompression: s.Config.EnableCompression,
 	}
 
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -69,6 +77,18 @@ func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	conn.SetReadLimit(s.Config.MaxMessageSize)
+	s.connections++
+	defer func() {
+		s.connections--
+		conn.Close()
+	}()
+
+	// Limit the number of concurrent connections by server config
+	if s.connections > s.Config.ConnectionLimit && s.Config.ConnectionLimit != 0 {
+		log.Printf("Connection limit exceeded: %v", s.connections)
+		conn.Close()
+		return
+	}
 
 	// Generate a unique connection ID for this connection
 	connectionId := uuid.New().String()
@@ -85,6 +105,10 @@ func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 	// Handle messages after sending the connection ID
 	for {
+		if s.Config.IdleTimeout != 0 {
+			conn.SetReadDeadline(time.Now().Add(time.Duration(s.Config.IdleTimeout) * time.Second))
+		}
+
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
